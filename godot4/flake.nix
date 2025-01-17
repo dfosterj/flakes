66646257d0ea265f0bcba711086810793921f236
dfosterj <dfosterj@hotmail.com>
2025-01-16 22:21:51 -0600

    add nixgl godot4

--- godot4/flake.nix
+++ godot4/flake.nix
@@ -1,39 +1,56 @@
 {
-  description = "Setup Godot 4 with Vulkan support for Intel GPUs via Nix";
+  description = "Setup Godot 4 with Vulkan support for Intel GPUs via NixGL (X11)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixgl.url = "github:nix-community/nixGL";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nixgl, lib }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+        mkdir $out
+        ln -s ${pkg}/* $out
+        rm $out/bin
+        mkdir $out/bin
+        for bin in ${pkg}/bin/*; do
+         wrapped_bin=$out/bin/$(basename $bin)
+         echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
+         chmod +x $wrapped_bin
+        done
+        '';
       in {
         devShell = pkgs.mkShell {
           name = "godot4-vulkan-intel-env";
 
           buildInputs = [
             pkgs.godot_4
-            pkgs.nixVulkanIntel
+            nixgl.nixVulkanIntel
+            pkgs.libglvnd
+            pkgs.xorg.libX11
+			(nixGLWrap pkgs.zenity)
           ];
 
           shellHook = ''
-            # Ensure Vulkan is properly configured
-            if [ -z "$VK_ICD_FILENAMES" ]; then
-              export VK_ICD_FILENAMES="/etc/vulkan/icd.d/intel_icd.json"
-            fi
+            # Set Vulkan ICD and Layer paths
+            export VK_ICD_FILENAMES=${nixgl.nixVulkanIntel}/etc/vulkan/icd.d/intel_icd.json
+            export VK_LAYER_PATH=${nixgl.nixVulkanIntel}/etc/vulkan/explicit_layer.d
 
-            echo "Starting Godot 4 with Vulkan support for Intel GPUs..."
+            # Force X11 (if Wayland is installed but unused)
+            export QT_QPA_PLATFORM="xcb"
+
+            echo "Starting Godot 4 with Vulkan support on X11 for Intel GPUs..."
           '';
         };
 
         # Define a default app for nix run
         apps.default = {
           type = "app";
-          program = "${pkgs.godot_4}/bin/godot";
-          args = [ "--vulkan" ];  # Force Vulkan mode in Godot 4
+          program = "${pkgs.godot_4}/bin/godot4";
+          args = [ "--vulkan" "--display-driver=x11" ];  # Use Vulkan with X11
         };
       }
     );
