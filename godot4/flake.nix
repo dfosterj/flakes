76780280729231b38fdf6160c190285c1eb59e7a
dfosterj <dfosterj@hotmail.com>
2025-01-16 23:02:36 -0600

    test args godot4

--- godot4/flake.nix
+++ godot4/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Setup Godot 4 with Vulkan support for Intel GPUs via NixGL (X11)";
+  description = "Setup Godot 4 with Vulkan support for Intel GPUs via NixGL (X11) and nixGLWrap";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -7,50 +7,60 @@
     nixgl.url = "github:nix-community/nixGL";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixgl, lib }:
+  outputs = { self, nixpkgs, flake-utils, nixgl }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+
+        # Wrapper function for executables using nixGL
         nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-        mkdir $out
-        ln -s ${pkg}/* $out
-        rm $out/bin
-        mkdir $out/bin
-        for bin in ${pkg}/bin/*; do
-         wrapped_bin=$out/bin/$(basename $bin)
-         echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
-         chmod +x $wrapped_bin
-        done
+        ┆ mkdir $out
+        ┆ ln -s ${pkg}/* $out
+        ┆ rm $out/bin
+        ┆ mkdir $out/bin
+        ┆ for bin in ${pkg}/bin/*; do
+        ┆ ┆wrapped_bin=$out/bin/$(basename $bin)
+        ┆ ┆echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
+        ┆ ┆chmod +x $wrapped_bin
+        ┆ done
         '';
+        nixVulkanIntelWrap = pkg: pkgs.runCommand "${pkg.name}-nixvulkanintel-wrapper" {} ''
+        ┆ mkdir $out
+        ┆ ln -s ${pkg}/* $out
+        ┆ rm $out/bin
+        ┆ mkdir $out/bin
+        ┆ for bin in ${pkg}/bin/*; do
+        ┆ ┆wrapped_bin=$out/bin/$(basename $bin)
+        ┆ ┆echo "exec ${lib.getExe nixgl.nixVulkanIntel} $bin \$@" > $wrapped_bin
+        ┆ ┆chmod +x $wrapped_bin
+        ┆ done
+        '';
+
       in {
         devShell = pkgs.mkShell {
           name = "godot4-vulkan-intel-env";
 
           buildInputs = [
-            pkgs.godot_4
+            nixgl.auto.nixGLDefault
             nixgl.nixVulkanIntel
+            pkgs.godot_4
             pkgs.libglvnd
+            pkgs.libglvnd
+            pkgs.wl-clipboard
+            pkgs.xwayland
             pkgs.xorg.libX11
-			(nixGLWrap pkgs.zenity)
+            (nixVulkanIntelWrap pkgs.godot_4)
+            (nixGLWrap pkgs.zenity)
           ];
 
-          shellHook = ''
-            # Set Vulkan ICD and Layer paths
-            export VK_ICD_FILENAMES=${nixgl.nixVulkanIntel}/etc/vulkan/icd.d/intel_icd.json
-            export VK_LAYER_PATH=${nixgl.nixVulkanIntel}/etc/vulkan/explicit_layer.d
-
-            # Force X11 (if Wayland is installed but unused)
-            export QT_QPA_PLATFORM="xcb"
-
-            echo "Starting Godot 4 with Vulkan support on X11 for Intel GPUs..."
-          '';
         };
 
         # Define a default app for nix run
         apps.default = {
           type = "app";
-          program = "${pkgs.godot_4}/bin/godot4";
-          args = [ "--vulkan" "--display-driver=x11" ];  # Use Vulkan with X11
+          program = "${pkgs.godot_4}/bin/godot4 ";
+          args = [ "--headless" "--export-release 'Linux/X11'" ];
         };
       }
     );
