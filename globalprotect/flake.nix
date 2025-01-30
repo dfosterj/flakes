1e7375ed3cfa867b36e1ac34d9cf6033e43179a3
dfosterj <dfosterj@hotmail.com>
2025-01-30 13:54:17 -0600

    rename gp

---
+++ globalprotect/flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Setup a VPN connection using gp-saml-gui with TLS/SSL support and openconnect integration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+      in {
+        # packages = import ./pkgs/top-level.nix {
+        #   callPackage = pkgs.callPackage;
+        #   lib = pkgs.lib;
+        #   pkgs = pkgs;
+        # };
+        devShell = pkgs.mkShell {
+          name = "vpn-gp-saml-gui-env";
+
+          buildInputs = [
+            pkgs.networkmanager
+            pkgs.gp-saml-gui
+            # pkgs.glib.networking
+            pkgs.gtk3
+            pkgs.libglvnd
+            pkgs.mesa
+            pkgs.openconnect
+          ];
+
+          shellHook = ''
+          echo "initializing globalprotect vpn..."
+          echo "Enter VPN url:"
+          read input
+          echo "You typed: $input"
+          '';
+          };
+      });
+}
+
