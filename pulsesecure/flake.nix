53f6e4c3869547147d5deb70d9f81f2bd2326f07
dfosterj <dfosterj@hotmail.com>
2025-02-12 12:49:01 -0600

    new pulse

--- pulsesecure/flake.nix
+++ pulsesecure/flake.nix
@@ -1,61 +1,65 @@
-{ pkgs, lib, ... }:
-let
-  pulse-cookie = pkgs.python3.pkgs.buildPythonApplication rec {
-    pname = "pulse-cookie";
-    version = "1.0";
+    pulse-cookie = pkgs.python3.pkgs.buildPythonApplication rec {
+      pname = "pulse-cookie";
+      version = "1.0";
 
-    src = pkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "sha256-ZURSXfChq2k8ktKO6nc6AuVaAMS3eOcFkiKahpq4ebU=";
+      src = pkgs.fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-ZURSXfChq2k8ktKO6nc6AuVaAMS3eOcFkiKahpq4ebU=";
+      };
+
+      propagatedBuildInputs = [
+        pkgs.python3.pkgs.pyqt6
+        pkgs.python3.pkgs.pyqt6-webengine
+        pkgs.python3.pkgs.setuptools
+        pkgs.python3.pkgs.setuptools_scm
+      ];
+
+      preBuild = ''
+        cat > setup.py << EOF
+        from setuptools import setup
+
+        setup(
+          name='pulse-cookie',
+          packages=['pulse_cookie'],
+          package_dir={"": 'src'},
+          version='1.0',
+          author='Raj Magesh Gauthaman',
+          description='wrapper around openconnect allowing user to log in through a webkit window for mfa',
+          install_requires=[
+            'PyQt6-WebEngine',
+          ],
+          entry_points={
+            'console_scripts': ['get-pulse-cookie=pulse_cookie._cli:main']
+          },
+        )
+        EOF
+      '';
+
+      meta = with pkgs.lib; {
+        homepage = "https://pypi.org/project/pulse-cookie/";
+        description = "wrapper around openconnect allowing user to log in through a webkit window for mfa";
+        license = licenses.gpl3;
+      };
     };
 
-    propagatedBuildInputs = [
-      pkgs.python3.pkgs.pyqt6
-      pkgs.python3.pkgs.pyqt6-webengine
-      pkgs.python3.pkgs.setuptools
-      pkgs.python3.pkgs.setuptools_scm
-    ];
-
-    preBuild = ''
-      cat > setup.py << EOF
-      from setuptools import setup
-
-      # with open('requirements.txt') as f:
-      #     install_requires = f.read().splitlines()
-
-      setup(
-        name='pulse-cookie',
-        packages=['pulse_cookie'],
-        package_dir={"": 'src'},
-        version='1.0',
-        author='Raj Magesh Gauthaman',
-        description='wrapper around openconnect allowing user to log in through a webkit window for mfa',
-        install_requires=[
-          'PyQt6-WebEngine',
-        ],
-        entry_points={
-          'console_scripts': ['get-pulse-cookie=pulse_cookie._cli:main']
-        },
-      )
-      EOF
+    start-pulse-vpn = pkgs.writeShellScriptBin "start-pulse-vpn" ''
+      HOST=https://secure.med.harvard.edu
+      DSID=$(${pulse-cookie}/bin/get-pulse-cookie -n DSID $HOST)
+      sudo ${pkgs.openconnect}/bin/openconnect --protocol nc -C DSID=$DSID $HOST
     '';
 
-    meta = with lib; {
-      homepage = "https://pypi.org/project/pulse-cookie/";
-      description = "wrapper around openconnect allowing user to log in through a webkit window for mfa";
-      license = licenses.gpl3;
-    };
-  };
-  start-pulse-vpn = pkgs.writeShellScriptBin "start-pulse-vpn" ''
-    HOST=https://secure.med.harvard.edu
-    DSID=$(${pulse-cookie}/bin/get-pulse-cookie -n DSID $HOST)
-    sudo ${pkgs.openconnect}/bin/openconnect --protocol nc -C DSID=$DSID $HOST
-  '';
+  in {
+    devShell = pkgs.mkShell {
+      name = "pulse-vpn-env";
 
-in
-{
-  environment.systemPackages = with pkgs; [
-    openconnect
-    start-pulse-vpn
-  ];
-}
+      buildInputs = [
+        pkgs.openconnect
+        pulse-cookie
+        start-pulse-vpn
+      ];
+
+      shellHook = ''
+        echo "Pulse VPN environment ready."
+      '';
+    };
+  });
